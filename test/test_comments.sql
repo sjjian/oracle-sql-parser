0f12cd881ba9b12da4e44b025264ba5004e5ba66
sjjian <921465802@qq.com>
2021-11-26 20:06:00 +0800

    support comments

---
+++ test/test_comments.sql
@@ -0,0 +1,23 @@
+--comment
+CREATE TABLE db1.t1 (
+    id int, -- comment
+    name varchar2(255) -- comment comment
+); -- !@#$%^&*(
+
+/*
+    comment comment comment
+*/
+CREATE TABLE db1.t1 (
+    /* comment */
+    id int,
+    name varchar2(255)
+);
+
+/*
+    comment comment comment
+*/
+CREATE TABLE db1.t1 (
+    /* comment */
+    id int, -- comment
+    name varchar2(255) -- comment comment
+); -- !@#$%^&*(
